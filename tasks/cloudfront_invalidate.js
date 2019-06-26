78c79e28ccad884e5ada61d0f0c2151e102f0d78
jasonmosser <jason@coschedule.com>
2019-06-26 17:42:59 -0500

    [CHORE] Update dependencies, config and author info.

--- tasks/cloudfront_invalidate.js
+++ tasks/cloudfront_invalidate.js
@@ -5,135 +5,136 @@
  * Copyright (c) 2015 cosrnos
  * Licensed under the MIT license.
  */
+const AWS = require('aws-sdk');
 
-'use strict';
+const description = 'A simple plugin to trigger a cloudfront invalidation';
 
-// Dependencies
-var AWS = require('aws-sdk');
-
-module.exports = function (grunt) {
-    grunt.registerMultiTask('cloudfront_invalidate', 'A simple plugin to trigger a cloudfront invalidation', function () {
-        var invalidateParams;
-        var done = this.async();
-        var options = this.options({
-            args: this.args,
-            reference: this.nameArgs
-        });
-
-        if (options.debug) {
-            grunt.log.ok(options.reference + ' options:', options);
-        }
-
-        if (!validateOptions(options, grunt)) {
-            return done(false);
-        }
-
-        var Config = new AWS.Config({
-            accessKeyId: options.accessKeyId,
-            secretAccessKey: options.secretAccessKey
-        });
-        var CloudFront = new AWS.CloudFront(Config);
-
-        invalidateParams = buildInvalidation(options, this.files);
-
-        if (!invalidateParams) {
-            grunt.log.error("Unable to build invalidation from given input.");
-            return done(false);
-        }
-
-        if (options.debug) {
-            grunt.log.ok(options.reference + ' invalidation:');
-            grunt.log.ok(invalidateParams);
-            return;
-        }
-
-        CloudFront.createInvalidation(invalidateParams, function (err/**, data **/) {
-            if (err) {
-                grunt.log.error("Error from AWS: ", (err && err.stack) || err);
-                return done(false);
-            }
-
-            grunt.log.ok(options.reference + " invalidation finished.");
-            return done();
-        });
+module.exports = function CloudFrontInvalidate(grunt) {
+  grunt.registerMultiTask('cloudfront_invalidate', description, function CloudFrontInvalidateTask() {
+    const done = this.async();
+    const options = this.options({
+      args: this.args,
+      reference: this.nameArgs,
     });
+
+    if (options.debug) {
+      grunt.log.ok(`${options.reference} options:`, options);
+    }
+
+    if (!validateOptions(options, grunt)) {
+      done(false);
+      return;
+    }
+
+    const Config = new AWS.Config({
+      accessKeyId: options.accessKeyId,
+      secretAccessKey: options.secretAccessKey,
+    });
+    const CloudFront = new AWS.CloudFront(Config);
+
+    const invalidation = buildInvalidation(options, this.files);
+
+    if (!invalidation) {
+      grunt.log.error('Unable to build invalidation from given input.');
+      done(false);
+      return;
+    }
+
+    if (options.debug) {
+      grunt.log.ok(`${options.reference} invalidation:`);
+      grunt.log.ok(invalidation);
+    }
+
+    CloudFront.createInvalidation(invalidation, (err/** , data * */) => {
+      if (err) {
+        grunt.log.error('Error from AWS: ', (err && err.stack) || err);
+        done(false);
+        return;
+      }
+
+      grunt.log.ok(`${options.reference} invalidation finished.`);
+      done();
+    });
+  });
 };
 
 function getBasicInvalidation(config) {
-    return {
-        DistributionId: config.distributionId,
-        InvalidationBatch: {
-            CallerReference: generateCallerReference(config),
-            Paths: {
-                Quantity: 0,
-                Items: []
-            }
-        }
-    };
+  return {
+    DistributionId: config.distributionId,
+    InvalidationBatch: {
+      CallerReference: generateCallerReference(config),
+      Paths: {
+        Quantity: 0,
+        Items: [],
+      },
+    },
+  };
 }
 
 function generateCallerReference(config) {
-    var ref = 'grunt_cloudfront_invalidate';
+  let ref = 'grunt_cloudfront_invalidate';
 
-    // Setup Reference
-    if (config.args && config.args.length) {
-        ref += '_' + config.args.join('_');
-    }
+  // Setup Reference
+  if (config.args && config.args.length) {
+    ref += `_${config.args.join('_')}`;
+  }
 
-    return ref + '_' + (+new Date());
+  return `${ref}_${+new Date()}`;
 }
 
 function addPathsFromConfig(invalidation, config) {
-    if (!config.path) {
-        return invalidation;
-    }
-
-    if (Array.isArray(config.path)) {
-        invalidation.InvalidationBatch.Paths.Items = invalidation.InvalidationBatch.Paths.Items.concat(config.path);
-        invalidation.InvalidationBatch.Paths.Quantity += config.path.length;
-    } else if (typeof config.path === 'string') {
-        addToBatch(invalidation, config.path);
-    }
-
+  if (!config.path) {
     return invalidation;
+  }
+
+  if (Array.isArray(config.path)) {
+    // eslint-disable-next-line no-param-reassign
+    invalidation.InvalidationBatch.Paths.Items = invalidation.InvalidationBatch.Paths.Items.concat(config.path);
+    // eslint-disable-next-line no-param-reassign
+    invalidation.InvalidationBatch.Paths.Quantity += config.path.length;
+  } else if (typeof config.path === 'string') {
+    addToBatch(invalidation, config.path);
+  }
+
+  return invalidation;
 }
 
 function addToBatch(invalidation, path) {
-    invalidation.InvalidationBatch.Paths.Items.push(path);
-    invalidation.InvalidationBatch.Paths.Quantity += 1;
-    return invalidation
+  invalidation.InvalidationBatch.Paths.Items.push(path);
+  // eslint-disable-next-line no-param-reassign
+  invalidation.InvalidationBatch.Paths.Quantity += 1;
+  return invalidation;
 }
 
 function addPathsFromFiles(invalidation, files) {
-    files.forEach(function (path) {
-        addToBatch(invalidation, path.dest);
-    });
+  files.forEach((path) => {
+    addToBatch(invalidation, path.dest);
+  });
 }
 
 function buildInvalidation(config, files) {
-    var invalidationConfig = getBasicInvalidation(config);
+  const invalidation = getBasicInvalidation(config);
 
-    addPathsFromConfig(invalidationConfig, config);
+  addPathsFromConfig(invalidation, config);
 
-    addPathsFromFiles(invalidationConfig, files);
+  addPathsFromFiles(invalidation, files);
 
-    return invalidationConfig;
+  return invalidation;
 }
 
 function validateOptions(options, grunt) {
-    var options_valid = true;
-    if (!options.accessKeyId) {
-        grunt.log.error('Please provide an accessKeyId');
-        options_valid = false;
-    }
-    if (!options.secretAccessKey) {
-        grunt.log.error('Please provide a secretAccessKey');
-        options_valid = false;
-    }
-    if (!options.distributionId) {
-        grunt.log.error('Please provide a distributionId');
-        options_valid = false;
-    }
+  if (!options.accessKeyId) {
+    grunt.log.error('Please provide an accessKeyId');
+    return false;
+  }
+  if (!options.secretAccessKey) {
+    grunt.log.error('Please provide a secretAccessKey');
+    return false;
+  }
+  if (!options.distributionId) {
+    grunt.log.error('Please provide a distributionId');
+    return false;
+  }
 
-    return options_valid;
+  return true;
 }
